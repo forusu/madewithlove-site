8c17b558b1689be3e09cbbcc23a66cd8299924e9
Mia Lane <27868690+forusu@users.noreply.github.com>
2021-02-28 09:44:32 +0100

    Fixed hearts

--- js.js
+++ js.js
@@ -1,7 +1,9 @@
-function swapStyleSheet(sheet, image, icon) {
+function swapStyleSheet(sheet, image, icon, hrt) {
     document.getElementById("style").setAttribute("href", sheet);
     img = document.getElementById("image").setAttribute("src", image)
     ic = document.getElementById("icon").setAttribute("href", icon)
+    hrt = document.getElementById("hrt").innerHTML = hrt
+    hrtf = document.getElementById("hrtf").innerHTML = hrt
 }
 
 function initate() {
@@ -9,11 +11,11 @@ function initate() {
     var style = document.getElementById("style");
 
     if ( style.getAttribute("href") == "index.css") {
-        heart.onclick = function () { swapStyleSheet("red.css", "images/prprd.webp", "https://discord.com/assets/0483f2b648dcc986d01385062052ae1c.svg"); };
+        heart.onclick = function () { swapStyleSheet("red.css", "images/prprd.webp", "https://discord.com/assets/0483f2b648dcc986d01385062052ae1c.svg", "❤"); };
     } else if (style.getAttribute("href") == "red.css") {
-        heart.onclick = function () { swapStyleSheet("cyan.css", "images/prpcy.webp", "https://discord.com/assets/e37c985edda06b7d5f4559bc838c1bde.svg"); };
+        heart.onclick = function () { swapStyleSheet("cyan.css", "images/prpcy.webp", "https://discord.com/assets/e37c985edda06b7d5f4559bc838c1bde.svg", "💙"); };
     } else if (style.getAttribute("href") == "cyan.css") {
-        heart.onclick = function () { swapStyleSheet("index.css", "images/prp.webp", "https://discord.com/assets/46dc70e2608d986da6de64c6ba5a59da.svg"); };
+        heart.onclick = function () { swapStyleSheet("index.css", "images/prp.webp", "https://discord.com/assets/46dc70e2608d986da6de64c6ba5a59da.svg", "💛"); };
     }
     
     
